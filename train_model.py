e6be03b9bf2067d18505b886f4a098cd227c6227
NickyBlackburn <nickblackburn02@gmail.com>
2023-08-21 14:00:50 -0400

    working on images for get

---
+++ train_model.py
@@ -0,0 +1,44 @@
+import numpy as np
+import tensorflow as tf
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, Flatten, Conv2D, MaxPooling2D
+
+# Load Dataset
+train_datagen = ImageDataGenerator(rescale=1./255, validation_split=0.2)
+train_generator = train_datagen.flow_from_directory(
+    'dataset/fursuit/',
+    target_size=(150, 150),
+    batch_size=32,
+    class_mode='categorical',
+    subset='training'
+)
+validation_generator = train_datagen.flow_from_directory(
+    'dataset/fursuit/',
+    target_size=(150, 150),
+    batch_size=32,
+    class_mode='categorical',
+    subset='validation'
+)
+
+# Build Model
+model = Sequential([
+    Conv2D(32, (3, 3), activation='relu', input_shape=(150, 150, 3)),
+    MaxPooling2D(2, 2),
+    Conv2D(64, (3, 3), activation='relu'),
+    MaxPooling2D(2, 2),
+    Flatten(),
+    Dense(512, activation='relu'),
+    Dense(train_generator.num_classes, activation='softmax')
+])
+
+# Compile and Train Model
+model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
+history = model.fit(
+    train_generator,
+    epochs=10,
+    validation_data=validation_generator
+)
+
+# Save the Model
+model.save('my_image_classifier.h5')
