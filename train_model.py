b37bd4b2a9f6c55ce2bda4b61c1d8d3aff7bc23a
NickyBlackburn <nickblackburn02@gmail.com>
2024-01-13 17:14:20 -0500

    Working on geting poetry and libs working  to try to get abasic tensorflow model

--- train_model.py
+++ train_model.py
@@ -7,14 +7,14 @@ from tensorflow.keras.layers import Dense, Flatten, Conv2D, MaxPooling2D
 # Load Dataset
 train_datagen = ImageDataGenerator(rescale=1./255, validation_split=0.2)
 train_generator = train_datagen.flow_from_directory(
-    'dataset/fursuit/',
+    'webscraper/dataset/fursuit/',
     target_size=(150, 150),
     batch_size=32,
     class_mode='categorical',
     subset='training'
 )
 validation_generator = train_datagen.flow_from_directory(
-    'dataset/fursuit/',
+    'webscraper/dataset/fursuit/',
     target_size=(150, 150),
     batch_size=32,
     class_mode='categorical',
